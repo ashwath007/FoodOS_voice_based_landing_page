1633732090cff548fc6ad005472313a78e8423e3
vicky <srivicky2000@gmail.com>
2021-10-28 14:08:59 +0530

    added server here

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8000
 
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
-
-
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
-
-app.get("/case", (req, res) => {
-    res.sendFile(path.join(__dirname, './public/case.html'));
-})
+// app.get("/case", (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/case.html'));
+// })
 
 app.listen(port, () => {
     console.log("Server Running ...");
